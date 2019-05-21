c5c80f547a61c97bcb6c48ec74b59bc689984f4a
Aleksa Sukovic <sukovic.aleksa@gmail.com>
2019-05-21 16:03:48 +0200

    Adds comments to RSAEncryptSection

--- src/Sections/RSAEncrypt/RSAEncryptSection.py
+++ src/Sections/RSAEncrypt/RSAEncryptSection.py
@@ -12,19 +12,23 @@ class RSAEncryptSection():
     def showPrompts(self):
         self.prompts.show()
 
+        # get data
         self.n = int(self.prompts.getAnswer('n'))
         self.e = int(self.prompts.getAnswer('e'))
         self.source = int(self.prompts.getAnswer('source'))
         self.outputType = self.prompts.getAnswer('outputType')
 
+        # read from keyboard
         if self.source == 1:
             self.content = self.prompts.showContentPrompt()['content']
 
+        # read from file
         if self.source == 2:
             path = self.prompts.showInputPathPrompt()['inputPath']
             with open(path) as fileContent:
                 self.content = fileContent.read()
 
+        # get output file path
         if self.outputType == 1:
             self.outputPath = self.prompts.showOutputPathPrompt()['outputPath']
 
